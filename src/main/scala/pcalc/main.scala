e17cb82a638acfeec39467031195e9ac5443aed4
Joe OpenSrc <joe-opensrc@protonmail.ch>
2018-05-17 21:50:39 +0200

    actual implentation

--- src/main/scala/pcalc/main.scala
+++ src/main/scala/pcalc/main.scala
@@ -1,8 +1,26 @@
 package pcalc
 
-object Foo extends App {
-  println("foobarz")
-  println("nice")
+object maths extends App {
+  def fact(n: BigInt): BigInt = {
+    if (n == 0) { return 1 } else { n * fact( n - 1) } 
+  }
+
+  def pnk( n: BigInt, k: BigInt ): BigInt = {
+    if ( k > n ) { return 0 } else {
+     return fact(n) / fact(n-k) 
+    }
+  }
+
+  def nck( n: BigInt, k: BigInt ): BigInt = {
+    if ( k > n ) { return 0 } else {
+     return pnk(n, k) / fact(k) 
+    }
+  }
+
+  printf( "fact(52) == %d\n", fact(52) )
+  printf( "pnk(5,2) == %d\n", pnk(5,2) )
+  printf( "nck(52,2) == %d\n", nck(52,2) )
+
 }
 
 
