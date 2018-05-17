71f5b30a9c36d3fe66a23cb60db986df9664ddad
Joe OpenSrc <joe-opensrc@protonmail.ch>
2018-05-17 22:08:29 +0200

    whitespaces

--- src/main/scala/pcalc/main.scala
+++ src/main/scala/pcalc/main.scala
@@ -2,7 +2,7 @@ package pcalc
 
 object maths extends App {
   def fact(n: BigInt): BigInt = {
-    if (n == 0) { return 1 } else { n * fact( n - 1) } 
+    if (n == 0) { return 1 } else { n * fact(n-1) } 
   }
 
   def pnk( n: BigInt, k: BigInt ): BigInt = {
@@ -17,10 +17,10 @@ object maths extends App {
     }
   }
 
+
   printf( "fact(52) == %d\n", fact(52) )
   printf( "pnk(5,2) == %d\n", pnk(5,2) )
   printf( "nck(52,2) == %d\n", nck(52,2) )
 
 }
-
 
