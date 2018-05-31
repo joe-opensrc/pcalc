bfcc59a7f620ae2b8489667e55e136f0ca203bb3
Joe OpenSrc <joe-opensrc@protonmail.ch>
2018-05-31 16:24:19 +0200

    big lump cos brain no worky

--- src/main/scala/pcalc/main.scala
+++ src/main/scala/pcalc/main.scala
@@ -5,11 +5,59 @@ package object types {
 
   type DeckSeqTuples = Seq[(Char,Char)]
   type Deck = Seq[Card]
-  type Board = Deck
+  type Board = Hand
+
 
 }
 
+package holdem.hands {
+  object types {
+    val HIGH_CARD       = 0 // a.b.c.d.z
+    val PAIR            = 1 // a.a.b.c.e
+    val TWO_PAIR        = 2 // a.a.b.b.c
+    val THREE_OF_A_KIND = 3 // a.a.a.b.c
+    val STRAIGHT        = 4 // a.b.c.d.e
+    val FLUSH           = 5 // A.A.A.A.A
+    val FULL_HOUSE      = 6 // a.a.a.b.b
+    val STRAIGHT_FLUSH  = 7 // A.B.C.D.E
+    val ROYAL_FLUSH     = 8 // V.W.X.Y.Z
 
+  }
+}
+
+
+object HandPivot {
+
+  def handType( h: Hand ): Int = {
+    /** flush **/
+    println( h.h.groupBy( _.schr ).mapValues(_.size).filter( _._2 >= 5 ))
+
+//    println( h.h.groupBy( _.fchr ).mapValues(_.size).filter( _._2 >= 2 ))
+    0
+  }
+
+  def handType( b: Board, h: Hand ): Int = {
+    h.merge(b)
+    h.sort()
+    println(h)
+    println( h.h.groupBy( _.fchr  ).mapValues(_.size).filter( _._2 >= 2 ))
+    0
+  }
+
+  /** TODO: length must be 5, or throw error **/
+  def detectStraight( h: Hand ) {
+     
+  }
+
+  def parseHand( h: Hand ): List[(Char,Int)] = {
+    val d = h.h.sorted
+    //♠♣♥♦    
+    List(('♠',3))
+  }
+
+}
+
+/** quite possibly make a HandFactory to implement game specific functions, i.e., hand ranking, number of cards per hand, etc **/
 class Hand() {
  
   private val _n: Int = 2
@@ -39,6 +87,15 @@ class Hand() {
     this._h = this._h ++ h.h
   }
 
+  /* don't really know what I'm doing here */
+  def rank( ) = {
+    //if ( h.lengh < 5 ){ 
+    //  throw Error()
+    //}
+    val h = this._h.sorted
+    h
+    
+  }
 
   def h = _h
 
@@ -76,14 +133,21 @@ object Dealer {
     return r
   }
 
+  /** TODO: define sort types **/
+  def sort( d: Deck ): Deck = {
+    d 
+  }
+
   def instance( start_type: Int = START_UNSHUFFLED ) = {
     new Dealer( start_type )
   }
 
 
+  def getHandType( h: Hand )  = {}
+
 }
 
-class Dealer() { 
+class Dealer() {  
 
   private var _deck: Deck = Dealer.newDeck
 
@@ -100,9 +164,12 @@ class Dealer() {
     this._deck = Dealer.shuffle( _deck ) 
   }
 
-  def deck: Deck = {
-    this._deck
+  def reset(): Unit = {
+    this._deck = Dealer.newDeck
   }
+
+  def deck = _deck
+//  def deck_= ( d: Deck ) = { this._deck = d }
 
   def deal( n: Int ): Hand = {
     val h = new Hand( this._deck.take(n) )
@@ -130,6 +197,10 @@ object CardPivot {
 
   def faceValue( c: Char ): Int = {
     return CardStringFaces.indexOf( c ) + 2
+  }
+
+  def faceToChar( i: Int ): Char = {
+    CardStringFaces(i-2)
   }
 
   def tupleToString( t: (Char,Char) ): String = {
@@ -177,8 +248,10 @@ class Card() extends Ordered [Card] {
   def i_= ( i: Int ) = { _i = i }
 
   def f = _f
+  def fchr = CardPivot.faceToChar(_f)
 
   def s = _s
+  def schr = this._srep(1)
 
   def this( s: String ) = {
     this()
@@ -242,6 +315,14 @@ object CardMaths {
     return (one to n).foldLeft(one)( (a,b) => (a*b) )
   }
 
+  def factors(num: BigInt) = {
+    val sr = scala.math.ceil( scala.math.sqrt(num.toDouble) ).toInt
+    ( 1 to sr ).filter { divisor =>
+      num % divisor == 0
+    }
+  }
+
+
   def pnk( n: BigInt, k: BigInt ): BigInt = {
     if ( k > n ) { return 0 } else {
      return fact(n) / fact(n-k) 
@@ -297,13 +378,72 @@ object CardMaths {
   final val RATIO_HANDS_POCKET_FLUSHDRAW: Double = 0.06862745098039215
 //  final val RATIO_HANDS_POCKET_
 
+
+  object holdem {
+    def rank( h: Hand ){
+      if ( h.length < 5 ){ 
+        println("Hand needs to at least 5 cards")
+      }else{
+        
+        // sort hand, 
+ 
+      }
+
+    }
+  }
+
+
+
+
   def main( args: Array[String] ): Unit = { 
 
-    var dealer = Dealer.instance(Dealer.START_SHUFFLED)
-    var deck   = dealer.deck
-    
-    printf("%s,%s,%s\n", dealer.deal(2), dealer.deal(2), dealer.deck ) 
+//println( CardMaths.factors( CardMaths.nck( 52, 5 )) )
 
+//println( scala.math.sqrt( CardMaths.nck( 52, 5 ).toDouble ))
+/** get hand, get sum, if straight compare indeces **/
+val sum_of_straights = Vector( 14, 2, 3, 4, 5 ).sum +: ( for ( i <- 2 to 10 ) yield { var x = for ( j <- 0 to 4 ) yield { i+j } ; x.sum } )  
+println( sum_of_straights )
+    var dealer = Dealer.instance(Dealer.START_SHUFFLED)
+
+//    for ( c <- dealer.deck ) { printf("%d,", CardPivot.cardToInt
+
+    // "♠♣♥♦"
+//    var h1 = new Hand("A♠|K♦")
+//    println( "h1: " + HandPivot.parseHand(h1) )
+//    var h2 = new Hand("A♣|A♥")
+//    h1.sort
+
+
+    val NUM_PLAYERS = 9 
+    val NUM_ROUNDS = 1000
+
+  for ( c <- 1 to NUM_ROUNDS ) { 
+
+ //     var h1 = new Hand("A♠|K♦")
+//      h1.sort
+//      dealer.removeHand( h1 ) 
+//    dealer.removeHand( h2 )
+   
+//    dealer.shuffle()
+   
+//    val other = for ( i <- 1 to NUM_PLAYERS) yield { val h = dealer.deal(2); h.sort(); h }    
+      var b: Hand = dealer.deal(7)
+      b.sort() 
+      println(b)
+//      println( HandPivot.handType(b,h1) )
+      HandPivot.handType( b )
+       
+//    printf("%s  %s  ", h1, b ) 
+//    val other = List()
+//    for ( h <- other ){ printf("%s ", h  ) } //dealer.deck)  
+
+//    println()
+
+    dealer.reset()
+    dealer.shuffle()
+//    dealer.removeHand(h1)
+//    printf("%s,%s,%s\n", dealer.deal(2), dealer.deal(3), dealer.deck ) 
+  }
   }
 }
 
