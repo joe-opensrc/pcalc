42c94a6de0f0bf3966582b38964b2ae18127bd28
Joe OpenSrc <joe-opensrc@protonmail.ch>
2018-06-01 14:44:22 +0200

    Rejig of Rank and Suit to use enumeratum

--- src/main/scala/pcalc/main.scala
+++ src/main/scala/pcalc/main.scala
@@ -8,38 +8,54 @@ package object types {
 
   
 
-sealed abstract class CardProperty( val c: Char, val v: Int ) extends Ordered[Rank] {
+
+sealed abstract class CardProperty( val c: Char, val v: Int ) extends EnumEntry with Ordered[CardProperty] {
 
   def toChar = c 
   def value = v
 
-  def compare( that: Rank ): Int = { this.v - that.v }
-  def  equals( that: Rank ): Boolean = { this.equals(that) } 
+  def compare( that: CardProperty ): Int = { this.v - that.v }
+  def  equals( that: CardProperty ): Boolean = { this.equals(that) } 
 
 }
+
 
 sealed abstract class Rank( override val c: Char, override val v: Int ) extends CardProperty( c,v ) {}
 sealed abstract class Suit( override val c: Char, override val v: Int ) extends CardProperty( c,v ) {}
 
-case object Two   extends Rank( '2',  2 )
-case object Three extends Rank( '3',  3 )
-case object Four  extends Rank( '4',  4 )
-case object Five  extends Rank( '5',  5 )
-case object Six   extends Rank( '6',  6 )
-case object Seven extends Rank( '7',  7 )
-case object Eight extends Rank( '8',  8 )
-case object Nine  extends Rank( '9',  9 )
-case object Ten   extends Rank( 'T', 10 )
-case object Jack  extends Rank( 'J', 11 )
-case object Queen extends Rank( 'Q', 12 )
-case object King  extends Rank( 'K', 13 )
-case object Ace   extends Rank( 'A', 14 )
 
-//♠♣♥♦
-case object Heart   extends Suit( '♥', 100 )
-case object Club    extends Suit( '♣', 100 )
-case object Diamond extends Suit( '♦', 100 )
-case object Spades  extends Suit( '♠', 100 )
+case object Rank extends Enum[Rank] {
+
+  val values = findValues
+
+  //23456789TJQKA
+  case object Two   extends Rank( '2',  2 )
+  case object Three extends Rank( '3',  3 )
+  case object Four  extends Rank( '4',  4 )
+  case object Five  extends Rank( '5',  5 )
+  case object Six   extends Rank( '6',  6 )
+  case object Seven extends Rank( '7',  7 )
+  case object Eight extends Rank( '8',  8 )
+  case object Nine  extends Rank( '9',  9 )
+  case object Ten   extends Rank( 'T', 10 )
+  case object Jack  extends Rank( 'J', 11 )
+  case object Queen extends Rank( 'Q', 12 )
+  case object King  extends Rank( 'K', 13 )
+  case object Ace   extends Rank( 'A', 14 )
+
+}
+
+case object Suit extends Enum[Suit] {
+
+  val values = findValues
+  
+  //♠♣♥♦
+  case object Heart   extends Suit( '♥', 100 )
+  case object Club    extends Suit( '♣', 100 )
+  case object Diamond extends Suit( '♦', 100 )
+  case object Spades  extends Suit( '♠', 100 )
+
+}
 
 class Card( private var _rank: Rank, private var _suit: Suit ) extends Ordered[Card]{
 
