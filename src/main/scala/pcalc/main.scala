85e133b77914348124240f7cc3f7ad22aa2d3fbe
Joe OpenSrc <joe-opensrc@protonmail.ch>
2018-06-06 22:36:59 +0200

    Enums are now PowersOfTwo to distinguish cards uniquely

--- src/main/scala/pcalc/main.scala
+++ src/main/scala/pcalc/main.scala
@@ -24,19 +24,19 @@ case object Rank extends Enum[Rank] {
   val values = findValues
 
   //23456789TJQKA
-  case object Two   extends Rank( '2',  2 )
-  case object Three extends Rank( '3',  3 )
-  case object Four  extends Rank( '4',  4 )
-  case object Five  extends Rank( '5',  5 )
-  case object Six   extends Rank( '6',  6 )
-  case object Seven extends Rank( '7',  7 )
-  case object Eight extends Rank( '8',  8 )
-  case object Nine  extends Rank( '9',  9 )
-  case object Ten   extends Rank( 'T', 10 )
-  case object Jack  extends Rank( 'J', 11 )
-  case object Queen extends Rank( 'Q', 12 )
-  case object King  extends Rank( 'K', 13 )
-  case object Ace   extends Rank( 'A', 14 )
+  case object Two    extends Rank( '2',    1 )
+  case object Three  extends Rank( '3',    2 )
+  case object Four   extends Rank( '4',    4 )
+  case object Five   extends Rank( '5',    8 )
+  case object Six    extends Rank( '6',   16 )
+  case object Seven  extends Rank( '7',   32 )
+  case object Eight  extends Rank( '8',   64 )
+  case object Nine   extends Rank( '9',  128 )
+  case object Ten    extends Rank( 'T',  256 )
+  case object Jack   extends Rank( 'J', 1024 )
+  case object Queen  extends Rank( 'Q', 2048 )
+  case object King   extends Rank( 'K', 4096 )
+  case object Ace    extends Rank( 'A', 8192 )
 
 }
 
@@ -45,10 +45,10 @@ case object Suit extends Enum[Suit] {
   val values = findValues
   
   //♠♣♥♦
-  case object Heart   extends Suit( '♥', 100 )
-  case object Club    extends Suit( '♣', 100 )
-  case object Diamond extends Suit( '♦', 100 )
-  case object Spade  extends Suit( '♠', 100 )
+  case object Heart   extends Suit( '♥', 16384 )
+  case object Club    extends Suit( '♣', 32768 )
+  case object Diamond extends Suit( '♦', 65536 )
+  case object Spade   extends Suit( '♠', 131072 )
 
 }
 
